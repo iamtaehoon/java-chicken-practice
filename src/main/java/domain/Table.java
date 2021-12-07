628f5b8953b530a32b92583fa3652e46e292d0af
iamtaehoon <kimth9981@naver.com>
2021-12-07 14:35:26 +0900

    feat(Table): 테이블에서 주문을 받는 기능 구현

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -2,9 +2,17 @@ package domain;
 
 public class Table {
     private final int number;
+    private Orders orders = new Orders();
 
     public Table(final int number) {
         this.number = number;
+    }
+
+    public void takeOrder(String menuInput, String orderCntInput) {
+        //메뉴와 주문개수를 여기서 만드는게 맞을까? 일단 만들기.
+        Menu menu = new Menu(1,"",Category.CHICKEN,1);
+        MenuCnt orderCnt = new MenuCnt(Integer.parseInt(orderCntInput)); //
+        orders.record(menu, orderCnt);
     }
 
     @Override
