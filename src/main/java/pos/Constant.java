7ee2304d78e1f7b2480df0d54969b6d3c696fb24
iamtaehoon <kimth9981@naver.com>
2021-12-07 20:30:34 +0900

    refactor(Constant): 숫자를 구별해주는 정규식 상수화

--- src/main/java/pos/Constant.java
+++ src/main/java/pos/Constant.java
@@ -3,4 +3,5 @@ package pos;
 public class Constant {
     public static int MIN_ORDER_CNT = 0;
     public static int MAX_ORDER_CNT = 99;
+    public static String INTEGER_REGEX = "-?\\d+";
 }
--- src/main/java/pos/InputValidator.java
+++ src/main/java/pos/InputValidator.java
@@ -1,8 +1,10 @@
 package pos;
 
+import static pos.Constant.INTEGER_REGEX;
+
 public class InputValidator {
     public static int validateNumber(String input) {
-        if (!input.matches("-?\\d+")) {
+        if (!input.matches(INTEGER_REGEX)) {
             throw new IllegalArgumentException("숫자를 입력해주세요.");
         }
         return Integer.parseInt(input);
