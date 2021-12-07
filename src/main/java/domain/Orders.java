b9ff2102b328b0b42011bee54bc12c215c3bcda0
iamtaehoon <kimth9981@naver.com>
2021-12-07 14:07:44 +0900

    feat(Orders): 주문을 받으면 orders의 해당 메뉴 개수를 갱신

---
+++ src/main/java/domain/Orders.java
@@ -0,0 +1,18 @@
+package domain;
+
+import java.util.LinkedHashMap;
+
+public class Orders { //주문서
+	private LinkedHashMap<Menu, MenuCnt> orders = new LinkedHashMap<Menu, MenuCnt>();
+
+	public void record(Menu menu, MenuCnt orderCnt) {
+		if (orders.containsKey(menu)) {
+			MenuCnt existingCnt = orders.get(menu);
+			MenuCnt totalCnt = MenuCnt.add(existingCnt, orderCnt);
+			orders.put(menu, totalCnt);
+			return;
+		}
+		orders.put(menu, orderCnt);
+		}
+	}
+}
